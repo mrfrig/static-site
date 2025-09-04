00a9423f28c6374bec953dfb42deedce5bf5652e
mrfrig <franklin@neosmail.com>
2025-09-04 18:09:51 -0400

    5-1 Add copy static to public function

---
+++ src/copy_static_to_public.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+
+static_path = "static"
+public_path = "public"
+
+
+def copy_static_to_public(path=static_path):
+    if path == static_path and os.path.exists(public_path) and os.listdir(public_path):
+        shutil.rmtree(public_path)
+
+    if not os.path.exists(public_path):
+        os.mkdir(public_path)
+
+    for name in os.listdir(path):
+        filepath = path + "/" + name
+        replacement = filepath.replace(f"{static_path}/", f"{public_path}/")
+        if os.path.isfile(filepath):
+            shutil.copy(filepath, replacement)
+        else:
+            os.mkdir(replacement)
+            copy_static_to_public(filepath)
--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
-from textnode import TextNode, TextType
+from copy_static_to_public import copy_static_to_public
 
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(node)
+    copy_static_to_public()
 
 
 if __name__ == "__main__":
